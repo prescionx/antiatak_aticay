f22fdb0c69e95fd5eb9ed0952332df7576757384
prescionx <prescionx@gmail.com>
2023-07-05 22:28:42 +0300

    Proje dosyası ekle.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApplication1
+{
+	// Token: 0x02000002 RID: 2
+	internal class Program
+	{
+		// Token: 0x06000001 RID: 1 RVA: 0x00002058 File Offset: 0x00000258
+		private static void Main(string[] args)
+		{
+			Process[] processes = Process.GetProcesses();
+			int num = 0;
+			foreach (Process process in processes)
+			{
+				if (process.ProcessName == "servisatk")
+				{
+					process.Kill();
+					num = 1;
+					break;
+				}
+				num = 0;
+			}
+			if (num == 1)
+			{
+				Environment.Exit(0);
+				return;
+			}
+			Environment.Exit(0);
+		}
+	}
+}
---
+++ ConsoleApplication1/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyCopyright("Copyright © PrescionX 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ea25891b-5d2a-4725-a486-b32b1f8974cb")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
